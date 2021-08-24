c01912f4dfca8ddc8bb2eedd00b295418f87f576
Corey <corey.amano@gmail.com>
2021-08-23 18:14:06 -0700

    add sorting to activerecord

--- app/controllers/kitchen_tickets_controller.rb
+++ app/controllers/kitchen_tickets_controller.rb
@@ -1,13 +1,13 @@
 class KitchenTicketsController < ApplicationController
   def index
-    ordered_items = OrderedItem.where(user_id: current_user.id, status: ["preparing"])
+    ordered_items = OrderedItem.where(status: ["preparing", "completed"]).order(status: :asc, created_at: :asc)
     render_items = []
     ordered_items.each do |ordered_item|
       render_item = ordered_item.as_json
       render_item["submitted_time"] = ordered_item.created_at.strftime("%b %e, %l:%M %p")
       render_items << render_item
     end
-    sorted_render_items = render_items.sort_by { |render_item| render_item["submitted_time"] }
+    sorted_render_items = render_items
     render json: sorted_render_items
   end
 end
